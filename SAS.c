3001ba973c243f2a4952446daacc47093c73d5d2
Aouatif Cherdid <awatifcherdid2468@gmail.com>
2025-10-01 07:19:04 +0200

    Add Gestion de Solde

--- SAS.c
+++ SAS.c
@@ -30,7 +30,13 @@ int main()
 {
        int choixMenu;
        int choixProfil;
+       int choixSolde;
+       float montant;
        Client client;
+
+       // Initialisation du client par défaut
+       client.idClient = 0;
+       client.solde = 0.0;
 
        do
        {
@@ -63,28 +69,30 @@ int main()
                             scanf("%s", client.prenom);
                             printf("Veuillez saisir votre nom: ");
                             scanf("%s", client.nom);
-                            sprintf(client.email,"%s.%s@client.com", client.prenom, client.nom);
+                            sprintf(client.email, "%s.%s@client.com", client.prenom, client.nom);
                             printf("Votre email genere est : %s\n", client.email);
-                            client.idClient = 0;
+                            client.idClient = 1;
                             client.solde = 0.00;
                             break;
+
                      case 2:
-                            
                             printf("Veuillez entrer votre nouveau prenom : ");
                             scanf("%s", client.prenom);
                             printf("Veuillez entrer votre nouveau nom : ");
-                            scanf("%s", client.nom);     
+                            scanf("%s", client.nom);
                             sprintf(client.email, "%s.%s@client.com", client.prenom, client.nom);
                             printf("Le profil est mis a jour.\n");
                             printf("Pour confirmation, votre email est : %s\n", client.email);
-                     case 3:
-                            printf("les informations du client sont: \n");
-                            printf(" Nom : %s\n", client.nom);
-                            printf(" Prenom : %s\n", client.prenom);   
-                            printf(" Email : %s\n", client.email); 
-                            printf("Solde actuel  :%f MAD\n:",client.solde);
-                            
                             break;
+
+                     case 3:
+                            printf("Les informations du client sont: \n");
+                            printf(" Nom : %s\n", client.nom);
+                            printf(" Prenom : %s\n", client.prenom);
+                            printf(" Email : %s\n", client.email);
+                            printf(" Solde actuel : %.2f MAD\n", client.solde);
+                            break;
+
                      default:
                             printf("Retour au menu principal.\n");
                             break;
@@ -92,41 +100,72 @@ int main()
                      break;
 
               case 2:
-                     printf("Consultation du solde \n");
-                     printf("Dépôt d'argent  \n");
-                     printf("Vérification automatique \n");
+                     printf("1. Consultation du solde \n");
+                     printf("2. Depot d'argent  \n");
+                     printf("3. Verification automatique \n");
+                     printf("0. Retour au menu principal\n");
+                     printf("Votre Choix : ");
+                     scanf("%d", &choixSolde);
 
-                     switch (choixProfil)
+                     switch (choixSolde)
                      {
                      case 1:
+                            printf("Votre solde actuel est : %.2f MAD\n", client.solde);
+                            break;
 
-                            printf("G \n");
-                            break;
                      case 2:
-                     printf("G \n");
-                            
-                     case 3:
-                            
+                            printf("Entrez le montant a deposer : ");
+                            scanf("%f", &montant);
+                            if (montant > 0)
+                            {
+                                   client.solde += montant;
+                            printf("Depot effectue. Nouveau solde : %.2f MAD\n", client.solde);
+                            }
+                            else
+                            {
+                                   printf("Montant invalide.\n");
+                            }
                             break;
-                     default:
+
+                     case 3:
+                            printf("Entrez le montant a verifier : ");
+                            scanf("%f", &montant);
+                            if (client.solde >= montant)
+                            {
+                                   printf("Solde suffisant (%.2f MAD) pour couvrir %.2f MAD.\n", client.solde, montant);
+                            }
+                            else
+                            {
+                                   printf("Solde insuffisant : %.2f MAD disponible, %.2f MAD necessaire.\n", client.solde, montant);
+                            }
+                            break;
+
+                     case 0:
                             printf("Retour au menu principal.\n");
+                            break;
+
+                     default:
+                            printf("Choix invalide !\n");
                             break;
                      }
                      break;
 
-                     break;
               case 3:
                      printf("Consultation des produits \n");
                      break;
+
               case 4:
                      printf("Effectuer un achat \n");
                      break;
+
               case 5:
                      printf("Mes statistiques \n");
                      break;
+
               case 0:
                      printf("Quitter l'application.\n");
                      break;
+
               default:
                      printf("Choix invalide !\n");
               }
